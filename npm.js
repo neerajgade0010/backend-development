c3c441fd45120d7165d426734e8da0a5519095b4
Neeraj Gade <91359584+neerajgade0010@users.noreply.github.com>
2025-10-07 18:44:34 +0000

    save

---
+++ npm.js
@@ -0,0 +1,14 @@
+// NOde.js = engine that runs javaScript outside the browser
+// NPM = is the store/ manager that gives Node.js extra tools (called packages )
+
+// NPM understanding 
+
+ // installing and uninstalling anything basics & advanced 
+  // install --> npm i package name
+  // uninstall --> npm uninstall packagename 
+
+  // what are dependencies --> packages and packages ki dependencies 
+  // devdependencies --> aise packages jo sirf develpoment main kam ayenge per jab app baan jayega 
+  //  aur upload ho jayega tab band ho jayega  
+  // development ke time pe hi use kerte hain as so as development is over after uploading it is not used 
+  
